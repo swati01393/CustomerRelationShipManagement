d5a75e2fd5011e9814f77813f62ea31bd49c8a3f
Pallavi <pallavi.d@bizee.in>
2019-12-16 16:06:11 +0530

    menupage

---
+++ src/Contact.js
@@ -0,0 +1,12 @@
+import React from'react';
+ import './Contactstyle.css';
+
+const Contact = () =>{
+    return(
+        <div className="contactstyle">
+            <h2>Welcome to contact page</h2>
+
+        </div>
+    )
+}
+export default Contact;
