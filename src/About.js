89a8f33f2d6f1bf50ec321f09a2ca0eed322c031
swati kumari <swati01393@gmail.com>
2020-01-20 15:20:51 +0530

    dashboard page

--- src/About.js
+++ src/About.js
@@ -1,12 +1,35 @@
-import React from'react';
- import './Aboutstyle.css';
+import React from 'react';
+import './Aboutstyle.css';
+
 
 const About = () =>{
     return(
-        <div className="aboutstyle">
-            <h2>Welcome to AboutUs page</h2>
 
+        <div className="aboutstyle">
+            <form className="formstyle">
+        <div>
+            
+            <div className="fst"> Run Status</div>
+            <div className=" snd">Controller</div>
+                    <div className="thrd">Power plant</div>
+                </div>
+           <div>
+                    <div className="frth">
+                        <h2>Control</h2></div>
+                    <div className="fth">
+                        <hr className="hrstyle" />
+                        Fault codencxmlvmlcmb
+                    <input type="text" placeholder="None" className="inputstyle" /> </div>
+                    <hr className="hrstyle" /><br/>
+                    <button > 0.v</button>
+                    <button > 0.v</button>
+                    <button > 0.v</button>
+                </div>  </form>
         </div>
-    )
+            
+
+
+   
+          )
 }
 export default About;
--- src/App.js
+++ src/App.js
@@ -1,48 +1,31 @@
 import React from 'react';
 import './App.css';
 import Menu from './Menu';
-import About   from './About';
+import Dashboard from './Dashboard';
+import About from './About';
+
 import Contact from './Contact';
-import {BrowserRouter , Route, Switch} from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
 
 
 
 function App() {
-  return (
-    <BrowserRouter>
-    <div>
-      <Menu/>
-      <Switch>
-      <Route path="/" exact component={Home}/>
-      <Route path="/about" component={About}/>
-      <Route path="/contact" component={Contact}/>
+    return (
+        <BrowserRouter>
+            <div>
+                <Menu/>
+                <Switch>
+                    <Route path="/" exact component={About} />
+                    <Route path="/about" component={Dashboard} />
+                    <Route path="/contact" component={Contact} />
+                    <Route path="/contact" component={Contact} />
 
-      </Switch>
-       
-    </div>
-    </BrowserRouter>
-  );
+                </Switch>
+
+            </div>
+        </BrowserRouter>
+    );
 }
-
-
-
-const Home = () =>{
-  return(
-      
-         <div className="wrap">
-      <form>
-        <h2>Login with</h2><hr></hr>
-      <label>User Name:-</label>
-      <input type="text" placeholder="Enter name here"></input><br/>
-      <label>Password:-</label>
-      <input type="text" placeholder="Enter password here"></input><br/>
-      <button>Submit</button></form>
-    </div>
-    
-
-  )
-}
-
-
 export default App;
 
